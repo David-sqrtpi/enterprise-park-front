61cecffdb7da223d0faf39e1b7302d31f89cb1b6
David <david.stiven911@gmail.com>
2022-06-28 01:08:57 -0500

    Crear el enrutamiento y las opciones

---
+++ src/routes/admin.js
@@ -0,0 +1,7 @@
+export default function Admin() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Administración</h2>
+        </main>
+    );
+}
---
+++ src/routes/analiticas.js
@@ -0,0 +1,7 @@
+export default function Analiticas() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Analíticas</h2>
+        </main>
+    );
+}
